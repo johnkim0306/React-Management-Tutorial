9f17495e308bd9ebb1c9328f1a235b84f3c405d2
johnkim0306 <mkcokr2176@gmail.com>
2019-02-19 20:47:07 -0700

    Create Cusomter.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+
+const customers = [
+{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'John',
+  'birthday': '19960306',
+  'gender': 'male',
+  'job': 'student'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'Kim',
+  'birthday': '19960306',
+  'gender': 'male',
+  'job': 'student'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'James',
+  'birthday': '19960306',
+  'gender': 'male',
+  'job': 'Designer'
+}
+
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's devlope managment system!</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+
       </div>
+
     );
   }
 }
